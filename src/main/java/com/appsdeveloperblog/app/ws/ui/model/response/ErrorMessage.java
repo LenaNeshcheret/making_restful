1c1d31449623fd01bec381750a6249564d9b6412
bigmag <awaywith2@icloud.com>
2023-02-02 18:27:21 +0100

    clean unused constructor

--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/ErrorMessage.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/response/ErrorMessage.java
@@ -6,8 +6,6 @@ public class ErrorMessage {
     private Date timestamp;
     private  String message;
 
-    public ErrorMessage(){}
-
     public ErrorMessage(Date timestamp, String message) {
         this.timestamp = timestamp;
         this.message = message;
